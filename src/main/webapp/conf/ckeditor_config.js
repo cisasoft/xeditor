4d63fc9b506d2f794045508f6c3dc2cdfcc95e79
Cisa <cisasoft@outlook.com>
2016-05-24 10:18:04 +0800

    google map

--- src/main/webapp/conf/ckeditor_config.js
+++ src/main/webapp/conf/ckeditor_config.js
@@ -6,5 +6,19 @@
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
+	// config.width = '75%';   // CSS unit (percent).
+	// config.height = '25em';  // CSS unit (em).
 	config.uiColor = '#6A6AFF';
+	config.width = 1000;     // 500 pixels wide.
+	config.height = 400;     // 500 pixels high.
+	
+	config.extraPlugins = 'clipboard';
+	config.extraPlugins = 'lineutils';
+	config.extraPlugins = 'widget';
+	
+	/* google map
+	config.extraPlugins = 'locationmap';
+	config.locationMapPath = './';
+	config.ckfinder = true;
+	*/
 };
